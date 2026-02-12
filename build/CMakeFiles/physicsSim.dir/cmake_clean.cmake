4d206e8aee07f0716e483df95060de1465220cf4
Navanya Sharma <sharmanavanya@gmail.com>
2026-02-12 12:55:40 +0530

    Ho Gaya

---
+++ build/CMakeFiles/physicsSim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/physicsSim.dir/Src/foo.cpp.o"
+  "CMakeFiles/physicsSim.dir/Src/foo.cpp.o.d"
+  "CMakeFiles/physicsSim.dir/main.cpp.o"
+  "CMakeFiles/physicsSim.dir/main.cpp.o.d"
+  "physicsSim"
+  "physicsSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/physicsSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/physicsSim.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for physicsSim.
+# This may be replaced when dependencies are built.
